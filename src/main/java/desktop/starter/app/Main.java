9d9697ab411fb6ba26e51718b8c7e8b444181b92
rob <rob@computer-1>
2021-06-22 11:41:17 +0300

    load resources from running folder, don't copy to jar; jar without dependencies

--- src/main/java/desktop/starter/app/Main.java
+++ src/main/java/desktop/starter/app/Main.java
@@ -7,7 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
@@ -16,10 +19,16 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-        ResourceBundle bundle = ResourceBundle.getBundle("bundles/lang",new Locale("ru"));
+//        ResourceBundle bundle = ResourceBundle.getBundle("bundles/lang",new Locale("ru"));
+    	ResourceBundle bundle = test();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"),bundle);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+    public static PropertyResourceBundle test() throws IOException {
+    	try (FileInputStream fis = new FileInputStream("bundles/lang_ru.properties")) {
+  		  return new PropertyResourceBundle(fis);
+  		}
     }
 }
