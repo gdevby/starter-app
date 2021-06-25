1f2680ab59afab8069c38bd59f950abd85002a73
rob <rob@computer-1>
2021-06-25 14:10:27 +0300

    changed resource logic

--- src/main/java/desktop/starter/app/Main.java
+++ src/main/java/desktop/starter/app/Main.java
@@ -1,17 +1,17 @@
 package desktop.starter.app;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Locale;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -20,8 +20,9 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
 //        ResourceBundle bundle = ResourceBundle.getBundle("bundles/lang",new Locale("ru"));
+    	System.out.println("dir " + new File("").getAbsolutePath());
     	ResourceBundle bundle = test();
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"),bundle);
+        Parent root = FXMLLoader.load(Paths.get("sample.fxml").toUri().toURL(),bundle);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
