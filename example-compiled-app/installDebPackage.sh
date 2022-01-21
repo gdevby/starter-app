b708a141194a759a5ad78bee8619c9368c575309
aleksandr <aleksandr@work-1>
2022-01-21 10:47:10 +0300

    update deb package and sh script

--- example-compiled-app/installDebPackage.sh
+++ example-compiled-app/installDebPackage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+sudo apt update
 sudo apt install openjdk-8-jre
 wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/bootstrapinstaller.deb
-chmod +x bootstrapinstaller.deb
 sudo dpkg -i bootstrapinstaller.deb
 sudo rm bootstrapinstaller.deb
-sudo rm installDebPackage.sh
