47c1eb2f4892e67f5e3cb10a1c85ab81d4b925b8
aleksandr <aleksandr@work-1>
2022-01-21 11:00:05 +0300

    changed made

--- example-compiled-app/installDebPackage.sh
+++ example-compiled-app/installDebPackage.sh
@@ -2,5 +2,6 @@
 sudo apt update
 sudo apt install openjdk-8-jre
 wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/bootstrapinstaller.deb
+sudo chmod +x bootstrapinstaller.deb
 sudo dpkg -i bootstrapinstaller.deb
 sudo rm bootstrapinstaller.deb
