6e65a9056e7cdcaab9ae079cc411502c63ffd579
aleksandr <aleksandr@work-1>
2022-03-01 16:12:36 +0300

    update installers

--- example-compiled-app/os_installer/installDebPackage.sh
+++ example-compiled-app/os_installer/installDebPackage.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install -y openjdk-8-jre
 sudo apt --fix-broken install
-wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/os_installer/bootstrapinstaller.deb
-sudo chmod +x bootstrapinstaller.deb
-sudo dpkg -i bootstrapinstaller.deb
-sudo rm bootstrapinstaller.deb
+wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/os_installer/starter.deb
+sudo chmod +x starter.deb
+sudo dpkg -i starter.deb
+sudo rm starter.deb
