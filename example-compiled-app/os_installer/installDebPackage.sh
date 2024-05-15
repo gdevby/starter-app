d0ff9f515f2e975fb3b197eb17ff1035305fce7d
alex <emanual014@gmail.com>
2024-05-15 10:45:51 +0300

    changed install deb script

--- example-compiled-app/os_installer/installDebPackage.sh
+++ example-compiled-app/os_installer/installDebPackage.sh
@@ -4,5 +4,6 @@ sudo apt install -y openjdk-8-jre
 sudo apt --fix-broken install
 wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/os_installer/starter.deb
 sudo chmod +x starter.deb
+sudo dpkg -P Starter
 sudo dpkg -i starter.deb
 sudo rm starter.deb
