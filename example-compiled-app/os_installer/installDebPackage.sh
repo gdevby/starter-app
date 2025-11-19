58e8f9299da3f25c90a9c6076cd936ac93010cce
Vadim Semashko <vadim.semashko05@gmail.com>
2025-11-19 18:48:10 +0000

    updated deb package files

--- example-compiled-app/os_installer/installDebPackage.sh
+++ example-compiled-app/os_installer/installDebPackage.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 sudo apt update
-sudo apt install -y openjdk-8-jre
+sudo apt install -y openjdk-21-jre
 sudo apt --fix-broken install
-wget https://raw.githubusercontent.com/gdevby/starter-app/master/example-compiled-app/os_installer/starter.deb
+wget https://raw.githubusercontent.com/gdevby/starter-app/java_21/example-compiled-app/os_installer/starter.deb
 sudo chmod +x starter.deb
-sudo dpkg -P Starter
 sudo dpkg -i starter.deb
 sudo rm starter.deb
