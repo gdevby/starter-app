dde462a788c012000810cd1dd486c0fd56f673b5
Vadim Semashko <vadim.semashko05@gmail.com>
2025-11-21 08:12:30 +0000

    added jre in deb package

--- example-compiled-app/os_installer/installDebPackage.sh
+++ example-compiled-app/os_installer/installDebPackage.sh
@@ -1,7 +1,4 @@
-sudo apt update
-sudo apt install -y openjdk-21-jre
-sudo apt --fix-broken install
-wget https://raw.githubusercontent.com/gdevby/starter-app/java_21/example-compiled-app/os_installer/starter.deb
+wget https://media.githubusercontent.com/media/gdevby/starter-app/java_21/example-compiled-app/os_installer/starter.deb
 sudo chmod +x starter.deb
 sudo dpkg -i starter.deb
 sudo rm starter.deb
